57daeb46b3bde0ca0e488289378d388ce12e1c2c
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2023-12-29 14:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const formElement = document.getElementById("form")
+
+formElement.addEventListener("submit", (event)=>{
+	event.preventDefault();
+	let data = {
+		firstName: formElement.firstName.value,
+		lastName: formElement.lastName.value,
+		phoneNumber: formElement.phoneNumber.value,
+		emailID: formElement.emailID.value,
+	}
+    var result = "First Name: " + data.firstName + "\nLast Name: " + data.lastName + "\nPhone Number: " + data.phoneNumber + "\nEmail ID: " + data.emailID;
+    alert(result);
+})
+
+
